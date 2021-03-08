ee0c2543bb0eb61cb4e253676324fdfe6a3bea95
Mohammed Fareed <71966959+mdfareed2001@users.noreply.github.com>
2021-03-08 12:17:06 +0530

    Update main.tf

--- terraform/backup/01-terraform-basics/main.tf
+++ terraform/backup/01-terraform-basics/main.tf
@@ -1,11 +1,22 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+ 
+# Configure the AWS Provider
 provider "aws" {
-    region = "us-east-1"
-    //version = "~> 2.46" (No longer necessary)
+  region = "us-east-1"
+  # VERSION IS NOT NEEDED HERE
 }
 
 # plan - execute 
 resource "aws_s3_bucket" "my_s3_bucket" {
-    bucket = "my-s3-bucket-in28minutes-rangak-002"
+    bucket = "my-s3-bucket-mdfareed2001-001"
     versioning {
         enabled = true
     }
